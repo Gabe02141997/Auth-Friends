3ad2269d9e423f0e8978c4aeb8355998c0408f44
Gabriel Anguiano <anguianog97@gmail.com>
2019-12-17 15:22:09 -0800

    added some styling

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,6 +1,24 @@
 import React,{useState } from 'react'
 import { axiosWithAuth } from '../utils/axiosWithAuth'
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+}));
+
+
 const Login = props => {
+    const classes = useStyles();
     const [loginForm, setLoginForm] = useState({ 
         username: '',
         password: ''
@@ -12,6 +30,7 @@ const Login = props => {
     }
 
 const login = e => {
+    console.log('hello')
     e.preventDefault(); 
     axiosWithAuth().post('/login', loginForm)
     .then(res => {
@@ -24,11 +43,13 @@ const login = e => {
 }
 
     return (
-        <div>
-            <form onSubmit={login}> 
-                <input placeholder='Username' name ='username' type='text' value={loginForm.username} onChange={changeHandler}/> 
-                <input placeholder='Password' type='password'  name ='password' value={loginForm.password} onChange={changeHandler}/> 
-                <button>Log In</button>
+        <div className='form-container'>
+            <form onSubmit={login} className='form'> 
+                <div className='input-container'>
+                <TextField id='outlined-basic' className={classes.textField} label='Username' margin='normal' variant='outlined'name ='username' type='text' value={loginForm.username} onChange={changeHandler}/> 
+                <TextField id='outlined-basic' className={classes.textField} label='Password' margin='normal' variant='outlined'name ='password' type='password' value={loginForm.password} onChange={changeHandler}/> 
+                </div>
+                <Button variant='contained' color='primary' type ='submit'onSubmit={login}>Log In</Button>
             </form>
         </div>
     )
--- friends/src/index.js
+++ friends/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom'
+import './styles.scss'
+
+
+
 
 ReactDOM.render(
 <Router><App /> </Router>, document.getElementById('root'));
